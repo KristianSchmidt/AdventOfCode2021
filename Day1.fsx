ad82dcff7f4270449e8af225583366c30a8bd55f
Kristian Schmidt <kristian@kreutz.us>
2021-12-01 06:03:28 +0100

    Day 1 solved

--- Day1.fsx
+++ Day1.fsx
@@ -6,12 +6,24 @@ Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
 let data = Helpers.Web.getInput 1
 
-let ans1 = data
+let ans1 =
+    data
+    |> Array.map int
+    |> Array.windowed 2
+    |> Array.filter (fun [|x1;x2|] -> x2 > x1)
+    |> Array.length
 
 ans1
 
 /// Part 2
 
-let ans2 = data
+let ans2 = 
+    data
+    |> Array.map int
+    |> Array.windowed 3
+    |> Array.map Array.sum
+    |> Array.windowed 2
+    |> Array.filter (fun [|x1;x2|] -> x2 > x1)
+    |> Array.length
 
 ans2

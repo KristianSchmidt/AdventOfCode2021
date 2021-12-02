487ed65aedb58b5f9f2dd2f224bdf5caf16f4126
Kristian Schmidt <kristian@kreutz.us>
2021-12-02 06:14:08 +0100

    Day 2 solution

--- Day2.fsx
+++ Day2.fsx
@@ -4,14 +4,41 @@ open System
 
 Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
-let data = Helpers.Web.getInput 2
+type Dir = | Forward of int
+           | Down of int 
+           | Up of int
 
-let ans1 = data
+let data =
+    Helpers.Web.getInput 2
+    |> Array.map (fun s -> match Helpers.split " " s with
+                           | [|"forward"; i|] -> Forward (int i)
+                           | [| "down"; i|] -> Down (int i)
+                           | [| "up"; i |] -> Up (int i)
+                           )
+
+let (x1,y1) =
+    data
+    |> Array.fold (fun (x,y) t -> match t with
+                                  | Forward i -> (x+i,y)
+                                  | Down i -> (x,y+i)
+                                  | Up i -> (x,y-i)
+                                  ) (0,0)
+
+let ans1 = x1*y1
 
 ans1
 
 /// Part 2
 
-let ans2 = data
+let (x2,y2,_) =
+    data
+    |> Array.fold (fun (x,y,a) t -> match t with
+                                    | Forward i -> (x+i,y+(a*i),a)
+                                    | Down i -> (x,y,a+i)
+                                    | Up i -> (x,y,a-i)
+                                  ) (0,0,0)
+
+
+let ans2 = (x2*y2)
 
 ans2

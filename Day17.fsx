473f728b87caa432d97f86af08f797b30f98fb2f
Kristian Schmidt <kristian@kreutz.us>
2021-12-17 06:43:55 +0100

    Day 17 solution

--- Day17.fsx
+++ Day17.fsx
@@ -6,12 +6,40 @@ Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
 let data = Helpers.Web.getInput 17
 
-let ans1 = data
+let (tx1,ty1) = (185,-122)
+let (tx2,ty2) = (221,-74)
 
-ans1
+let doIter (pos : int*int,vel : int*int) =
+    match (pos,vel) with
+    | (pX,pY),(vX,vY) ->
+        (pX+vX, pY+vY),(vX-Math.Sign(vX),vY-1)
 
-/// Part 2
+let makePath start vel' =
+    let rec f (pos,vel) path =
+        let (newPos,newVel) = doIter (pos,vel)
+        match newPos with
+        | (x,y) when x >= tx1 && x <= tx2 && y >= ty1 && y <= ty2 ->
+            Some (vel',newPos :: path)
+        | (_,y) when y < ty1 -> None
+        | (x,_) when x > tx2 -> None
+        | _ ->
+            f (newPos,newVel) (newPos :: path)
+    
+    f (start,vel') []
+    
+let vels =
+    seq {
+        for x in 0..tx2 do
+            for y in ty1..1000 do
+                yield makePath (0,0) (x,y)
+    }
+    |> Array.ofSeq
+    |> Array.choose id
 
-let ans2 = data
+let ans1 =
+    vels
+    |> Array.map (snd >> List.maxBy snd)
+    |> Array.maxBy snd
+    |> snd
 
-ans2
+let ans2 = vels.Length

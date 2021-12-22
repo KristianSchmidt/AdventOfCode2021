623249ffb6ba690a3df9d6b8244a2241a1e6aa65
Kristian Schmidt <kristian@kreutz.us>
2021-12-22 20:37:17 +0100

    Day 22 solution

--- Day22.fsx
+++ Day22.fsx
@@ -24,12 +24,11 @@ let switchOnOff m (st,xmin, xmax, ymin, ymax, zmin, zmax) =
 
     map
 
-data
-|> Array.fold switchOnOff Map.empty
-|> Map.filter (fun k v -> v = "on")
-|> Map.count
-
-let ans1 = data
+let ans1 =
+    data
+    |> Array.fold switchOnOff Map.empty
+    |> Map.filter (fun k v -> v = "on")
+    |> Map.count
 
 ans1
 
@@ -40,69 +39,64 @@ let data2 =
     |> Array.map (fun (st,xmin, xmax, ymin, ymax, zmin, zmax) -> (st, int64 xmin, int64 xmax, int64 ymin, int64 ymax, int64 zmin, int64 zmax))
 
 let axisOverlap (a1min,a1max,a2min,a2max) =
-    if (a2min <= a1min && a1max <= a2max) then
-        Some (a2min,a2max)
-    else if (a1min <= a2min && a2max <= a1max) then
-        Some (a1min,a1max)
-    else if (a2min <= a1max && a1min <= a2max) then
-        Some (a2min, a1max)
-    else if (a1min <= a2max && a2min <= a1max) then
-        Some (a1min, a2max)
-    else
+    //printfn "%A" (a1min,a1max,a2min,a2max)
+    if (a2min > a1max || a1min > a2max) then
         None
-        
-let prod (c : (int64*int64) option) =
-    match c with
-    | Some (amin,amax) -> (amax-amin)
-    | None -> 0L
-
+    else
+        Some (max a1min a2min, min a1max a2max)
+    
 let isOverlap ((st1,xmin1, xmax1, ymin1, ymax1, zmin1, zmax1),(st2,xmin2, xmax2, ymin2, ymax2, zmin2, zmax2)) =
     let xOverlap = axisOverlap (xmin1, xmax1, xmin2, xmax2)
-        //max (min (xmax1 - xmin2) (xmax2 - xmin1)) 0L
     let yOverlap = axisOverlap (ymin1, ymax1, ymin2, ymax2)
-        //max (min (ymax1 - ymin2) (ymax2 - ymin1)) 0L
     let zOverlap = axisOverlap (zmin1, zmax1, zmin2, zmax2)
-        //max (min (zmax1 - zmin2) (zmax2 - zmin1)) 0L
 
     //printfn "x: %A. y: %A. z: %A" xOverlap yOverlap zOverlap
 
-    let overlap = (prod xOverlap) * (prod yOverlap) * (prod zOverlap)
-    if (overlap > 0L) then
-        let (oxmin,oxmax) = if ((xmax1 - xmin2) > (xmax2 - xmin1)) then (xmin1, xmax2) else (xmin2, xmax1)
-        let (oymin,oymax) = if ((ymax1 - ymin2) > (ymax2 - ymin1)) then (ymin1, ymax2) else (ymin2, ymax1)
-        let (ozmin,ozmax) = if ((zmax1 - zmin2) > (zmax2 - zmin1)) then (zmin1, zmax2) else (zmin2, zmax1)
-        Some (oxmin,oxmax,oymin,oymax,ozmin,ozmax)
-    else
-        None
+    match xOverlap, yOverlap, zOverlap with
+    | Some (xomin,xomax), Some (yomin,yomax), Some (zomin,zomax) ->
+        let st = match st1,st2 with
+                 | "on","on" -> "off"
+                 | "off", "on" -> "on"
+                 | "off","off" -> "on"
+                 | "on", "off" -> "off"
+        
+        Some (st,xomin,xomax,yomin,yomax,zomin,zomax)
+    | _ -> None
 
+let area (_,xmin,xmax,ymin,ymax,zmin,zmax) =
+    ((xmax-xmin) + 1L) *
+    ((ymax-ymin) + 1L) *
+    ((zmax-zmin) + 1L)
 
-isOverlap (("", 0L,1L,0L,1L,1L,2L),("", 0L,1L,0L,1L,0L,4L))
+let getSt (st,_,_,_,_,_,_) = st
 
-Array.allPairs data2 data2
-|> Array.filter (fun (p1,p2) -> p1 <> p2)
-|> Array.map (fun (p1,p2) -> (p1,p2),isOverlap (p1,p2))
-|> Array.filter (snd >> Option.isSome)
-|> Array.countBy (fst >> fst)
-|> Array.sortByDescending snd
+let solve data =
+    let rec f processed queue =
+        match queue with
+        | [] -> processed
+        | x :: xs ->
+            let overlaps =
+                processed
+                |> List.choose (fun p -> isOverlap (p, x))
+            //printfn "x: %A" x
+            //printfn "Overlaps: %A" overlaps
+            let newProcessed =
+                if (getSt x = "off") then
+                    List.concat [overlaps;processed]
+                else
+                    List.concat [[x];overlaps;processed]
+            f newProcessed xs
 
-let ans2 = data
+    f [] (List.ofArray data)
+
+let ans2 =
+    let m =
+        solve data2
+        |> List.map (fun c -> c, area c)
+        |> List.map (fun ((st,_,_,_,_,_,_),a) -> st, a)
+        |> List.groupBy fst 
+        |> List.map (fun (k,v) -> k, v |> List.sumBy snd)
+        |> Map.ofList
+    m["on"] - m["off"]
 
 ans2
-
-//
-//         xmin1    xmax1
-//     xmin2    xmax2
-//
-//
-//       xmin1    xmax1
-//          xmin2    xmax2
-(*
-((("on", -30L, 14L, -7L, 40L, -48L, 6L),
-  ("off", 10L, 22L, 22L, 39L, -30L, -19L)),
- Some (10L, 14L, 22L, 40L, -48L, -19L));
-
-
-
-
-
-*)
